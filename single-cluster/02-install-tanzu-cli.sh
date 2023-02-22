634b178f0c8c108666d2c0ed5ca806482dae7c50
Viktor <viktorvdberg@yahoo.com>
2023-02-22 08:46:53 +0100

    update

--- single-cluster/02-install-tanzu-cli.sh
+++ single-cluster/02-install-tanzu-cli.sh
@@ -11,7 +11,7 @@ tar -xvf $TANZU_CLI_FILE -C $HOME/tanzu
 cd $HOME/tanzu
 #use the parameter TANUZ_CLI_VERSION to set VERSION (and follow the format as shared in the TAP documentation)
 export VERSION=$TANZU_CLI_VERSION
-install cli/core/$VERSION/tanzu-core-darwin_amd64 /usr/local/bin/tanzu
+sudo install cli/core/$VERSION/tanzu-core-darwin_amd64 /usr/local/bin/tanzu
 tanzu plugin install --local cli all
 tanzu plugin list
 tanzu version
