ebff417a754bcfc20a66d5f71ab56e0989e2f1b2
Viktor van den Berg <viktorvdberg@yahoo.com>
2023-05-11 18:36:43 +0200

    update

--- single-cluster/09-tap-configure-tls.sh
+++ single-cluster/09-tap-configure-tls.sh
@@ -1,7 +1,8 @@
 # 09-tap-configure-tls.sh
 #
-
+# First update password.txt and route53-clusterissuer.yaml
 #
 #
-kubectl apply -f  ./additional-configuration/testing-tekton/default-tests-production.yaml -n $PRODUCTION_NAMESPACE
-kubectl apply -f  ./additional-configuration/testing-tekton/default-tests-iterate.yaml -n $ITERATE_NAMESPACE
+kubectl create secret generic route53-credentials-secret --from-file=./additional-configuration/tls/password.txt -n cert-manager
+kubectl apply -f ./additional-configuration/tls/route53-clusterissuer.yaml
+kubectl apply -f ./additional-configuration/tls/tap-gui-certificate.yaml
