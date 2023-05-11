e0a41ade4aa3a21ac04697ea3b8c8db417ae4094
Viktor van den Berg <viktorvdberg@yahoo.com>
2023-05-11 18:40:02 +0200

    update

--- single-cluster/10-add-tekton-testing.sh
+++ single-cluster/10-add-tekton-testing.sh
@@ -5,3 +5,6 @@
 #
 kubectl apply -f  ./additional-configuration/testing-tekton/default-tests-production.yaml -n $PRODUCTION_NAMESPACE
 kubectl apply -f  ./additional-configuration/testing-tekton/default-tests-iterate.yaml -n $ITERATE_NAMESPACE
+
+# Optional, add java specific test
+kubectl apply -f  ./additional-configuration/testing-tekton/java-test-pipeline.yaml -n #PUT A NAMESPACE HERE
